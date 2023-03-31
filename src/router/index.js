1ae7b7643ad1ff2202e5829dcb2ea42e591963ec
Felix <felix.kuhbier@yahoo.com>
2023-03-31 19:53:51 +0200

    removed path

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,6 @@ import Home from '../views/Home.vue'
 
 const routes = [
     { path: '/', name: 'Home', component: Home },
-    { path: '/contact', name: 'Contact', component: () => import('../views/Contact.vue') },
     { path: '/about', name: 'About', component: () => import('../views/About.vue') }
 ]
 
