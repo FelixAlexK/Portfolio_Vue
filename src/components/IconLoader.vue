512f807fc937bb7a685a31df8ee36907672ff151
FelixAlexK <fkuhbier@stud.hs-heilbronn.de>
2023-04-08 02:04:54 +0200

    added icons

---
+++ src/components/IconLoader.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { defineAsyncComponent } from 'vue';
+
+const props = defineProps({
+    name: {
+        type: String,
+        required: true,
+    },
+});
+
+const icon = defineAsyncComponent(() =>
+    import(`../assets/icons/dev_icons/${props.name}`)
+);
+</script>
+
+<template>
+    <component :is="icon" class="fill-current" />
+</template>
+
+<style scoped>
+icon {
+
+    display: block;
+    max-width: 100%;
+    width: 2rem;
+    height: auto;
+
+}
+</style>
