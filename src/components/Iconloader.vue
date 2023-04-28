3c197f42b585306c0de234725d50ec606e816c3c
FelixAlexK <fkuhbier@stud.hs-heilbronn.de>
2023-04-28 11:39:56 +0200

    created component

---
+++ src/components/Iconloader.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref, defineAsyncComponent } from 'vue'
+
+const props = defineProps({
+    name: {
+        type: String,
+        required: true
+    }
+})
+
+const icon = defineAsyncComponent(() =>
+
+    import(`../assets/icons/dev_icons/${props.name}.svg`)
+
+)
+
+
+</script>
+
+<template>
+    <component :is="icon" class="fill-current" />
+</template>
