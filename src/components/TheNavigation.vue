601ce02b340e614a0549fa0fef48468aa8d5aadd
Felix.K <107888450+FelixAlexK@users.noreply.github.com>
2023-06-22 16:07:16 +0200

    changed design

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -1,16 +1,15 @@
 <template>
-    <div
-        class="w-full fixed h-24 text-text flex justify-center items-center z-10 shadow-2xl p-8 ">
+    <div class="w-full fixed h-24 text-text flex justify-center items-center z-10 shadow-2xl p-8 ">
         <div class="flex justify-between items-center w-3/5">
             <div
-                class="flex  text-text font-lato text-6xl tracking-wider hover:text-accent hover:transition-all ease-in-out scale-95">
+                class="flex  text-text font-inter font-bold text-6xl tracking-wider hover:text-accent hover:transition-all ease-in-out hover:scale-95">
                 <router-link to="/">
                     <h1>FK</h1>
                 </router-link>
             </div>
 
             <div
-                class="text-2xl inline-block text-text hover:underline opacity-70 transition-all ease-in-out router-link-active hover:opacity-100 ">
+                class="text-2xl inline-block text-text hover:underline opacity-70 transition-all ease-in-out hover:opacity-100 ">
                 <router-link to="/about">Über mich</router-link>
             </div>
         </div>
