05534f09c45ac0fce0d51a6a7f804cc614b33d45
Felix.K <107888450+FelixAlexK@users.noreply.github.com>
2023-06-22 13:57:28 +0200

    changed design

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -1,15 +1,18 @@
 <template>
-    <div class="w-full bg-slate-900 fixed h-24 text-slate-50 flex items-center justify-between z-10 shadow-2xl p-8 ">
-        <div
-            class="text-slate-50 font-lato text-6xl tracking-wider hover:text-emerald-300 hover:transition-all ease-in-out scale-95">
-            <router-link to="/">
-                <h1>FK</h1>
-            </router-link>
-        </div>
+    <div
+        class="w-full fixed h-24 text-text flex justify-center items-center z-10 shadow-2xl p-8 ">
+        <div class="flex justify-between items-center w-3/5">
+            <div
+                class="flex  text-text font-lato text-6xl tracking-wider hover:text-accent hover:transition-all ease-in-out scale-95">
+                <router-link to="/">
+                    <h1>FK</h1>
+                </router-link>
+            </div>
 
-        <div
-            class="text-2xl hover:text-emerald-300 inline-block text-slate-50 hover:underline opacity-70 transition-all ease-in-out router-link-active hover:opacity-100 ">
-            <router-link to="/about">Über mich</router-link>
+            <div
+                class="text-2xl inline-block text-text hover:underline opacity-70 transition-all ease-in-out router-link-active hover:opacity-100 ">
+                <router-link to="/about">Über mich</router-link>
+            </div>
         </div>
     </div>
 </template>
