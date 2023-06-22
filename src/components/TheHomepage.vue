d17d64e0a1a03dabe98d25736f1898ceda920329
Felix.K <107888450+FelixAlexK@users.noreply.github.com>
2023-06-22 13:56:45 +0200

    changed design

--- src/components/TheHomepage.vue
+++ src/components/TheHomepage.vue
@@ -3,24 +3,25 @@
 </script>
 
 <template>
-    <section class="w-full h-3/4 flex justify-center items-center bg-slate-900">
-        <article class=" w-4/5  flex justify-center items-center flex-col">
-
-            <h1 class="text-slate-50 font-crimson font-bold text-6xl mb-8">Hi, ich bin Felix!</h1>
-            <div class="block text-center text-2xl  text-slate-50 font-inter max-w-2xl">
+    <section class="w-full flex justify-center h-2/3 bg-background">
+        <article class=" m-52 pt-8 flex justify-center items-center flex-col max-w-full gap-8">
+            <h1 class="text-text font-crimson font-bold text-6xl">Hi, ich bin Felix!</h1>
+            <div class="block text-center text-2xl text-text font-inter max-w-2xl">
                 <p>
-                    <strong>Student</strong>
-                    an der <a class="hover:text-emerald-300 hover:underline transition-all ease-linear "
-                        href=" https://www.hs-heilbronn.de/de" title="hs-heilbronn.de" target="_blank">Hochschule
+                    <strong class="text-accent">Student</strong>
+                    an der <a class=" hover:underline transition-all ease-linear " href=" https://www.hs-heilbronn.de/de"
+                        title="hs-heilbronn.de" target="_blank">Hochschule
                         Heilbronn.</a>
                     <br>
-                    Ich habe eine große Leidenschaft für alles rund um die Technik und finde es unglaublich spannend,
+                    Ich habe eine große Leidenschaft für alles rund um die Technik und finde es unglaublich
+                    spannend,
                     mich
                     damit auseinanderzusetzen.
                 </p>
                 <br>
                 <p>
-                    Ich bin stets offen für neue Lernmöglichkeiten, besonders wenn es um technologische Entwicklungen
+                    Ich bin stets offen für neue Lernmöglichkeiten, besonders wenn es um technologische
+                    Entwicklungen
                     geht.
                 </p>
             </div>
