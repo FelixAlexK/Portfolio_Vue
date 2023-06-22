5ddd137dbd48567f11610b9b1b3efda312097114
Felix.K <107888450+FelixAlexK@users.noreply.github.com>
2023-06-22 13:54:46 +0200

    added font and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,15 @@ export default {
       crimson: ['Crimson Pro', 'serif'],
       lato: ['Lato', 'sans-serif'],
       inter: ['Inter', 'sans-serif'],
+      syne: ['Syne', 'sans-serif']
+    },
+    colors: {
+      'text': '#ffffff',
+      'background': '#070b2c',
+      'primary-button': '#c71f30',
+      'secondary-button': '#090e39',
+      'accent': '#30c71f',
+      'secondary-button-500': '#15193B',
     },
     extend: {},
   },
