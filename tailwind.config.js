a2006f4c0de9da13061d2850a7806865e1574992
Felix.K <107888450+FelixAlexK@users.noreply.github.com>
2023-06-22 22:41:13 +0200

    added screens and light colors and flowbite plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,16 +11,33 @@ export default {
       inter: ['Inter', 'sans-serif'],
       syne: ['Syne', 'sans-serif']
     },
+    screens: {
+      'mobiles': '320px',
+      'mobilem': '375px',
+      'mobilel': '425px',
+      'tablet': '768px',
+      'laptop': '1024px',
+      'laptopnl': '1440px'
+    },
     colors: {
       'text': '#ffffff',
+      'text-light': '#020302',
       'background': '#070b2c',
+      'background-light': '#f7f8f7',
       'primary-button': '#c71f30',
+      'primary-button-light': '#82737e',
       'secondary-button': '#090e39',
+      'secondary-button-light': '#ffffff',
       'accent': '#30c71f',
-      'secondary-button-500': '#15193B',
+      'accent-light': '#4c434a',
+      'card': '#15193B',
+      'card-light': '#D6D5D6'
     },
+    darkMode: 'class',
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
 
