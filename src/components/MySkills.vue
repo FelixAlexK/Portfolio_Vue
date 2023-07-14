635f1aca6c586f0d2b7d2d4259a81edecfaccaae
Felix.K <107888450+FelixAlexK@users.noreply.github.com>
2023-07-14 04:03:46 +0200

    changed design, added accordion and progressbar

--- src/components/MySkills.vue
+++ src/components/MySkills.vue
@@ -10,26 +10,49 @@ import Icon from '../components/TheIconloader.vue'
     <section class="flex justify-center w-full min-h-full">
         <article class="flex flex-col items-start py-8  w-11/12">
             <span
-                class="flex dark:bg-primary-button bg-primary-button-light shadow-2xl dark:hover:shadow-primary-button hover:shadow-primary-button-light dark:text-text text-text-light rounded-full py-1 px-4 tracking-widest uppercase align-middle whitespace-nowrap text-xl lg:text-3xl font-medium">Skills</span>
-            <div class="grid grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-8 py-6 ">
+                class="flex dark:bg-primary-button bg-primary-button-light shadow-2xl dark:hover:shadow-primary-button hover:shadow-primary-button-light 
+                dark:text-text text-text-light rounded-full py-1 px-4 tracking-widest uppercase align-middle whitespace-nowrap text-xl lg:text-3xl font-medium font-inter">Skills</span>
+            <div class="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-8 py-6 w-full">
                 <div v-for="skill in skills" :key="skill.id"
-                    class="w-full dark:bg-card bg-card-light rounded-lg border-b-4  shadow-2xl transition-all  ease-in-out hover:scale-95 focus:scale-95 "
+                    class="max-w-2xl dark:bg-card bg-card-light rounded-lg border-b-4 shadow-2xl  transition-all ease-in-out hover:scale-95 focus:scale-95 h-fit"
                     :style="{ borderColor: skill.color }">
-                    <div class="row-auto col-auto">
 
-                        <div class="flex items-end justify-end m-2">
-                            <Icon class="block w-10 h-auto dark:fill-text fill-text-light opacity-30 hover:opacity-100" :name="skill.icon_path">
+                    <div class="flex flex-row-reverse justify-between items-center m-6">
+                        <div class="flex">
+                            <Icon class="block w-14 aspect-auto h-auto dark:fill-text fill-text-light "
+                                :name="skill.icon_path">
                             </Icon>
                         </div>
-                        <div class="p-4">
-                            <h3 class=" transition-all ease-in-out text-base lg:text-lg 2xl:text-2xl dark:text-text text-text-light  ">{{ skill.name }}
+                        <div>
+                            <h3
+                                class="transition-all ease-in-out text-base lg:text-lg 2xl:text-2xl dark:text-text text-text-light font-crimson">
+                                {{ skill.name }}
                             </h3>
-                            <p class="text-sm lg:text-base 2xl:text-xl transition-all ease-in-out dark:text-text text-text-light opacity-70">{{
-                                skill.category }}</p>
+                            <p
+                                class="text-sm lg:text-base 2xl:text-xl transition-all ease-in-out dark:text-text text-text-light opacity-70 font-inter">
+                                {{ skill.category }}</p>
                         </div>
 
-
                     </div>
+
+                    <div class="m-6">
+                        <details class="group">
+                            <summary class="flex justify-end items-center font-medium cursor-pointer list-none">
+                                <span class="transition group-open:rotate-180 ">
+                                    <Icon class=" block w-6 aspect-auto h-auto dark:fill-text fill-text-light rotate-90"
+                                        :name="'arrow'"></Icon>
+                                </span>
+                            </summary>
+                            <div class="w-full mt-8  bg-gray-200 rounded-full dark:bg-gray-700">
+                                <div class=" flex justify-center h-6 rounded-full text-text font-inter"
+                                    :style="{ width: skill.rate + '%', backgroundColor: skill.color }">
+                                    {{ skill.rate }}%
+                                </div>
+                            </div>
+
+                        </details>
+                    </div>
+
                 </div>
             </div>
 
