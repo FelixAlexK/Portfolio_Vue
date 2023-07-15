c30c04755151c73f7917a8854949cdf4adbf4034
Felix.K <107888450+FelixAlexK@users.noreply.github.com>
2023-07-15 12:56:30 +0200

    changed layout

--- src/views/TheAbout.vue
+++ src/views/TheAbout.vue
@@ -7,39 +7,32 @@
 
 <template>
     <section
-        class="w-full  min-h-screen before:bg-gradient-to-t flex dark:bg-background  from-secondary-button-light to-background-light">
-        <article class="w-11/12 m-8 py-32 2xl:py-0 flex flex-col 2xl:justify-center items-start ">
-            <span
-                class="flex dark:bg-primary-button bg-primary-button-light shadow-2xl dark:hover:shadow-primary-button hover:shadow-primary-button-light dark:text-text text-text-light rounded-full py-1 px-4 tracking-widest uppercase align-middle whitespace-nowrap text-xl lg:text-3xl font-medium font-inter">
-                Über mich</span>
+        class="flex w-full justify-center min-h-full bg-gradient-to-t dark:from-secondary-button dark:to-background  from-secondary-button-light to-background-light bg-no-repeat bg-cover">
+        <article class="flex py-8 w-11/12">
 
-            <div class="flex flex-col lg:p 2xl:flex-row 2xl:items-center gap-8 py-6 ">
-                <img class="block max-w-xs lg:max-w-md 2xl:max-w-lg aspect-square rounded-3xl object-cover object-center "
-                    src="../assets/img/IMG_4706.jpeg" alt="logo" loading="lazy" format="jpeg">
+            <div class="w-full flex flex-col justify-center items-center py-36 gap-8">
+                <img class="block w-max lg:max-w-md 2xl:max-w-lg aspect-square h-auto rounded-3xl object-cover object-center "
+                    src="../assets/img/IMG_4706.jpeg" alt="image" loading="lazy" format="jpeg">
 
+                <h1 class=" font-bold font-crimson text-3xl lg:text-5xl 2xl:text-7xl dark:text-text text-text-light">
+                    Hi, ich bin
+                    Felix</h1>
+                <p
+                    class="block text-center text-lg lg:text-2xl 2xl:text-4xl dark:text-text text-text-light font-inter w-full lg:w-2/3 2xl:w-4/5 ">
+                    ich komme aus einer kleinen Gemeinde im Schwarzwald. Als Student im 4. Semester an der Hochschule
+                    Heilbronn begeistere ich mich seit meiner Kindheit für die Welt der Computer
+                    und der Programmierung. Es erfüllt mich mit Freude und Motivation, jeden Tag mehr darüber zu lernen.
+                    Neben meinem Studium unternehme ich gerne etwas mit Freunden, gehe essen, zocke und koche gerne.
+                    Durch mein Studium habe ich bereits zahlreiche kleine Projekte umgesetzt,
+                    bei denen ich wertvolle Erfahrungen in der Software-Entwicklung und der Zusammenarbeit im Team
+                    sammeln
+                    konnte.
+                    Diese Praxiserfahrung hilft mir, meine Kenntnisse zu vertiefen und mich kontinuierlich
+                    weiterzuentwickeln.
+                    Ich bin immer auf der Suche nach neuen Herausforderungen und Möglichkeiten,
+                    um meine Fähigkeiten zu verbessern.
+                </p>
 
-
-                <div class="flex flex-col gap-8">
-                    <h1
-                        class="flex font-bold  font-crimson text-3xl lg:text-5xl 2xl:text-7xl dark:text-text text-text-light">
-                        Hi, ich bin
-                        Felix</h1>
-                    <p
-                        class=" block text-left font-inter text-lg lg:text-2xl 2xl:text-3xl dark:text-text text-text-light w-full lg:w-4/5 2xl:w-3/4">
-                        ich komme aus einer kleinen Gemeinde im Schwarzwald. Als Student im 4. Semester an der Hochschule
-                        Heilbronn begeistere ich mich seit meiner Kindheit für die Welt der Computer
-                        und der Programmierung. Es erfüllt mich mit Freude und Motivation, jeden Tag mehr darüber zu lernen.
-                        Neben meinem Studium unternehme ich gerne etwas mit Freunden, gehe essen, zocke und koche gerne.
-                        Durch mein Studium habe ich bereits zahlreiche kleine Projekte umgesetzt,
-                        bei denen ich wertvolle Erfahrungen in der Software-Entwicklung und der Zusammenarbeit im Team
-                        sammeln
-                        konnte.
-                        Diese Praxiserfahrung hilft mir, meine Kenntnisse zu vertiefen und mich kontinuierlich
-                        weiterzuentwickeln.
-                        Ich bin immer auf der Suche nach neuen Herausforderungen und Möglichkeiten,
-                        um meine Fähigkeiten zu verbessern.
-                    </p>
-                </div>
 
 
             </div>
