89c2d74ed2962e912fc174161b2758baebfd78de
Felix.K <107888450+FelixAlexK@users.noreply.github.com>
2023-07-19 22:39:27 +0200

    added nav-dots

--- src/views/TheHome.vue
+++ src/views/TheHome.vue
@@ -1,18 +1,31 @@
 <script setup>
-//import GitHubProject from '../components/GitHubProject.vue';
-import svgLoader from 'vite-svg-loader';
-import GitHubProject from '../components/GitHubProject.vue';
-import SkillLandingPage from '../components/MySkills.vue'
-import AboutMeLandingPage from '../components/TheHomepage.vue';
+import GitHubProject from '../components/GitHubSection.vue';
+import SkillLandingPage from '../components/SkillSection.vue'
+import AboutMeLandingPage from '../components/LandingSection.vue';
+import Icon from '../components/TheIconloader.vue'
 </script>
 
 <template>
-    <div class="h-full overflow-hidden">
-        <AboutMeLandingPage></AboutMeLandingPage>
-        <div
-            class="bg-gradient-to-t dark:from-secondary-button dark:to-background from-secondary-button-light to-background-light flex flex-col bg-no-repeat bg-cover">
-            <SkillLandingPage></SkillLandingPage>
-            <GitHubProject></GitHubProject>
+    <div class="flex h-full overflow-hidden">
+        <div>
+            <AboutMeLandingPage></AboutMeLandingPage>
+            <div
+                class="bg-gradient-to-t dark:from-secondary-button dark:to-background from-secondary-button-light to-background-light flex flex-col bg-no-repeat bg-cover">
+                <SkillLandingPage></SkillLandingPage>
+                <GitHubProject></GitHubProject>
+            </div>
+        </div>
+
+        <div class="fixed right-0 top-1/2 -translate-y-1/2 z-10 grid mr-5 invisible lg:visible">
+            <a href="#landing" v-smooth-scroll>
+                <Icon :name="'dot'" class="block w-14 aspect-auto h-auto dark:fill-accent fill-accent-light "></Icon>
+            </a>
+            <a href="#skill" v-smooth-scroll>
+                <Icon :name="'dot'" class="block w-14 aspect-auto h-auto dark:fill-accent fill-accent-light "></Icon>
+            </a>
+            <a href="#github" v-smooth-scroll>
+                <Icon :name="'dot'" class="block w-14 aspect-auto h-auto dark:fill-accent fill-accent-light "></Icon>
+            </a>
         </div>
     </div>
 </template>
